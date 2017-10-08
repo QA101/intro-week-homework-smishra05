d43f0e8f4d5eb217bf805603489ae0ed75781d29
dpaulkinney <32225331+dpaulkinney@users.noreply.github.com>
2017-10-07 20:42:46 -0700

    committing my story

---
+++ src/MyStory.java
@@ -0,0 +1,28 @@
+
+public class MyStory {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("My name is Dustin Kinney");
+		System.out.println("In my spare time, I play video games.");
+		System.out.println("Currently, I am playing Dungeon of the Endless");
+		System.out.println();
+		System.out.println("I also read comics");
+		System.out.println("I am finishing up the New 52 storyline for Batman");
+		System.out.println();
+		System.out.println("I have lived all of over the US and travel around the world several times");
+		System.out.println();
+		System.out.println("Most recently, I lived in Boise for the last 7 years before moving to Seattle");
+		System.out.println();
+		System.out.println("Something that is fun about programming is that you can make the code very hard to read in the IDE");
+		System.out.print("bu\tx\b\b\b\b\b\b\bt much easier in the console\b\t\b");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
