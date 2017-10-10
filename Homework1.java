874c07334acbbac18a2acbe0e07de9651ed632b6
smishra05 <32606987+smishra05@users.noreply.github.com>
2017-10-09 20:13:27 -0700

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,19 @@
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am Soumya Mishra");
+		System.out.println("I am attending the QA101 class - Selenium course");
+		System.out.println("I am interested in being a QA for Alexa");
+		System.out.println("Apart from work, I love to travel");
+		System.out.println("I am originally from Delhi, India");
+		System.out.println("I enjoy eating mexican and Indian cuisine");
+		System.out.println("I like to code in Java");
+		System.out.println("I do love to swim");
+		System.out.println("My favorite animal is a Lion");
+		
+	}
+
+}
